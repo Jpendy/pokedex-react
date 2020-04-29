d6938f15dca8661189f9e1584c35d77098527027
Jpendy <Jpendy256@gmail.com>
2020-04-29 15:07:06 -0700

    added input and search button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  state = { searchQuery: null }
+
+    handleChange = (event) => {
+      const value = event.target.value;
+      this.setState( {searchQuery: value} );
+  }
+
+  handleClick = () => {
+    console.log('hello')
+  }
+
+  render() {
+
+    return (
+      <div>
+        
+        <input onChange={ this.handleChange } ></input>
+        <button onClick={ this.handleClick }>Search</button>
+
+
+      </div>
+    )
+  }
 }
-
-export default App;
