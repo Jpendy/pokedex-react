035f40ad2f573950ffa4467c6394fb7479f37711
Jpendy <Jpendy256@gmail.com>
2020-04-29 16:02:45 -0700

    got rendering to work from api data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react'
+import request from 'superagent';
 
 export default class App extends Component {
 
-  state = { searchQuery: null }
+  state = { searchQuery: null,
+            data: '' }
 
     handleChange = (event) => {
       const value = event.target.value;
       this.setState( {searchQuery: value} );
   }
 
-  handleClick = () => {
-    console.log('hello')
+  handleClick = async () => {
+      const requestedData = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=${this.state.searchQuery}`);
+
+      // console.log(requestedData.body.results[0])
+      this.setState({ data: requestedData.body.results[0] })
   }
 
   render() {
-
+    console.log(this.state.data)
     return (
       <div>
         
         <input onChange={ this.handleChange } ></input>
+
         <button onClick={ this.handleClick }>Search</button>
 
+
+      <h1> this is a test {this.state.data.pokemon} </h1>
 
       </div>
     )
